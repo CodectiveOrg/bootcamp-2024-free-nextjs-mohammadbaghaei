9323e6da0afb89b3d5f4be86ded53d4e1997e317
MOHAMMAD BAGHAEI <49115397+mohammadbaghaei@users.noreply.github.com>
2025-01-01 12:21:44 +0330

    Lesson 2 (#3)
    * complete lesson 2
    
    * add github workflows, remove unnecessary comment
    
    * install prettier
    
    * add prettier scripts to package.json and format codes with prettier

---
+++ src/components/header/header.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import clsx from "clsx";
+import styles from "./header.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactElement } from "react";
+
+type navLink = {
+  title: string;
+  href: string;
+};
+
+const navLinks: navLink[] = [
+  {
+    title: "خانه",
+    href: "/",
+  },
+  {
+    title: "پزشکان",
+    href: "/doctors",
+  },
+  {
+    title: "درباره ما",
+    href: "/about",
+  },
+];
+
+export default function Header(): ReactElement {
+  const pathname = usePathname();
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          {navLinks.map(({ title, href }) => (
+            <li key={title}>
+              <Link
+                className={clsx(href === pathname && styles.active)}
+                href={href}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <button className={styles.cta}>ورود | ثبت نام</button>
+    </header>
+  );
+}
